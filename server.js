68b4e4fe990642236d79c73bea1a83f8c7347fd4
Razvan <razzvan.savu@gmail.com>
2017-04-11 14:40:02 +0000

    Intepret url parameter

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
+"use strict";
+
 var express = require('express')
 var app = express()
 const port = 8080
 
+function analyzeDate(date) {
+  return {unix: Date.parse(date), natural: date}
+}
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
+})
+
+app.get('/:data', (req, res) => {
+  let data = req.params.data
+  res.send(analyzeDate(data))
 })
 
 app.listen(port, function () {
